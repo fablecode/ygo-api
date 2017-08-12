e083cca6e06d05f928991a9f129f72d1caae25b9
fablecode <fablecode@users.noreply.github.com>
2017-08-12 16:23:39 +0100

    ygo.database.scripts recreated.

---
+++ src/Database/ygo.database.scripts/dbo/Tables/Categories.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Categories] (
+    [Id]      BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Name]    NVARCHAR (255) NOT NULL,
+    [Created] DATETIME       NOT NULL,
+    [Updated] DATETIME       NOT NULL,
+    CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
