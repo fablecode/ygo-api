99f2d541a88cb6328a5c1d17ccd00bbf7e5d8164
fablecode <fablecode@users.noreply.github.com>
2017-08-12 16:21:36 +0100

    ygo.database.scripts delete.

--- src/Database/ygo.Database.Scripts/dbo/Tables/Limits.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Limits] (
-    [Id]          BIGINT         IDENTITY (1, 1) NOT NULL,
-    [Name]        NVARCHAR (255) NOT NULL,
-    [Description] NVARCHAR (MAX) NOT NULL,
-    [Created]     DATETIME       NOT NULL,
-    [Updated]     DATETIME       NOT NULL,
-    CONSTRAINT [PK_Statuses] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
--- src/Database/ygo.database.scripts/dbo/Tables/Categories.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Categories] (
-    [Id]      BIGINT         IDENTITY (1, 1) NOT NULL,
-    [Name]    NVARCHAR (255) NOT NULL,
-    [Created] DATETIME       NOT NULL,
-    [Updated] DATETIME       NOT NULL,
-    CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
