7647153c79c38e4958d3a7b861367dc3011c362f
fablecode <fablecode@users.noreply.github.com>
2017-08-25 08:34:25 +0100

    Microsoft.AspNetCore.Rewrite nuget packaged added.

--- src/Presentation/ygo.api/Startup.cs
+++ src/Presentation/ygo.api/Startup.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
@@ -49,6 +50,9 @@ namespace ygo.api
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ygo API V1");
             });
+
+            app.UseRewriter(new RewriteOptions()
+                .AddRedirect("/", "swagger"));
         }
     }
 }
