d04d1e88ad657d5aba8ae954cea26bb3b3f9e021
fablecode <fablecode@users.noreply.github.com>
2017-11-09 22:05:58 +0000

    LatestBanlistQueryValidator add to test solution.

---
+++ tests/unit/ygo.application.unit.tests/QueriesTests/LatestBanlistQueryHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ygo.application.unit.tests.QueriesTests
+{
+    [TestClass]
+   public class LatestBanlistQueryHandlerTests
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+            var sut = new LatestBanlistQueryHandler();
+        }
+
+
+    }
+
+    public class LatestBanlistQueryHandler
+    {
+        public LatestBanlistQueryHandler()
+        {
+        }
+    }
+}
---
+++ tests/unit/ygo.application.unit.tests/ValidatorsTests/Queries/LatestBanlistByFormatQueryValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ygo.application.unit.tests.ValidatorsTests.Queries
+{
+    [TestClass]
+    public class LatestBanlistByFormatQueryValidatorTests
+    {
+        private LatestBanlistQueryValidator _sut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _sut = new LatestBanlistQueryValidator();
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow(" ")]
+        public void Given_An_Format_Acronym_Validation_Should_Fail(string format)
+        {
+            // Arrange
+            var query = new LatestBanlistQuery { Format = format };
+
+            // Act
+            Action act = () => _sut.ShouldHaveValidationErrorFor(lbl => lbl.Format, query);
+
+            // Assert
+            act.Invoke();
+        }
+
+    }
+
+    public class LatestBanlistQueryValidator : AbstractValidator<LatestBanlistQuery>
+    {
+        public LatestBanlistQueryValidator()
+        {
+            RuleFor(lb => lb.Format)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+
+    public class LatestBanlistQuery
+    {
+        public string Format { get; set; }
+    }
+}
