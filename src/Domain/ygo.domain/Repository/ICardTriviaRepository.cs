b6759753c7bff641f96a76f9af855760ad025e0a
fablecode <fablecode@users.noreply.github.com>
2018-06-03 15:15:27 +0100

    Repository for card trivia added.

---
+++ src/Domain/ygo.domain/Repository/ICardTriviaRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ygo.core.Models.Db;
+
+namespace ygo.domain.Repository
+{
+    public interface ICardTriviaRepository
+    {
+        Task<List<TriviaSection>> TriviaSectionsByCardId(long cardId);
+        Task DeleteByCardId(long cardId);
+        Task Update(List<TriviaSection> tipSections);
+    }
+}
---
+++ src/Infrastructure/ygo.infrastructure/Repository/CardTriviaRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ygo.core.Models.Db;
+using ygo.domain.Repository;
+using ygo.infrastructure.Database;
+
+namespace ygo.infrastructure.Repository
+{
+    public class CardTriviaRepository : ICardTriviaRepository
+    {
+        private readonly YgoDbContext _context;
+
+        public CardTriviaRepository(YgoDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<TriviaSection>> TriviaSectionsByCardId(long cardId)
+        {
+            return _context
+                .TriviaSection
+                    .Include(t => t.Card)
+                    .Include(t => t.Trivia)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task DeleteByCardId(long cardId)
+        {
+            var triviaSections = await _context
+                                .TriviaSection
+                                .Include(t => t.Card)
+                                .Include(t => t.Trivia)
+                                .Where(ts => ts.CardId == cardId)
+                                .ToListAsync();
+
+            if (triviaSections.Any())
+            {
+                _context.Trivia.RemoveRange(triviaSections.SelectMany(t => t.Trivia));
+                _context.TriviaSection.RemoveRange(triviaSections);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task Update(List<TriviaSection> tipSections)
+        {
+            _context.TriviaSection.UpdateRange(tipSections);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
