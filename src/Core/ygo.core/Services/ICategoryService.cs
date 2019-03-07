d50e7420e3759fa27025ddd737780c99e95fdcdf
fablecode <fablecode@users.noreply.github.com>
2019-03-07 15:38:09 +0000

    CategoryService added to solution.

---
+++ src/Core/ygo.core/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ygo.core.Models.Db;
+
+namespace ygo.core.Services
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> AllCategories();
+        Task<Category> CategoryById(int id);
+        Task<Category> Add(Category category);
+    }
+}
---
+++ src/Domain/ygo.domain/Services/CategoryService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ygo.core.Models.Db;
+using ygo.core.Services;
+
+namespace ygo.domain.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryService(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public Task<List<Category>> AllCategories()
+        {
+            return _categoryService.AllCategories();
+        }
+
+        public Task<Category> CategoryById(int id)
+        {
+            return _categoryService.CategoryById(id);
+        }
+
+        public Task<Category> Add(Category category)
+        {
+            return _categoryService.Add(category);
+        }
+    }
+}
