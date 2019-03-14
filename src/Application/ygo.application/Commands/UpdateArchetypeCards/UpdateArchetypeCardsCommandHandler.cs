dae819ce7378539adaae8338f58c8da87fe0e5e0
fablecode <fablecode@users.noreply.github.com>
2019-03-14 10:08:25 +0000

    UpdateArchetypeCardsCommandHandler code coverage.

--- src/Application/ygo.application/Commands/UpdateArchetypeCards/UpdateArchetypeCardsCommandHandler.cs
+++ src/Application/ygo.application/Commands/UpdateArchetypeCards/UpdateArchetypeCardsCommandHandler.cs
@@ -3,19 +3,19 @@ using MediatR;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ygo.domain.Repository;
+using ygo.core.Services;
 
 namespace ygo.application.Commands.UpdateArchetypeCards
 {
     public class UpdateArchetypeCardsCommandHandler : IRequestHandler<UpdateArchetypeCardsCommand, CommandResult>
     {
         private readonly IValidator<UpdateArchetypeCardsCommand> _validator;
-        private readonly IArchetypeCardsRepository _archetypeCardsRepository;
+        private readonly IArchetypeCardsService _archetypeCardsService;
 
-        public UpdateArchetypeCardsCommandHandler(IValidator<UpdateArchetypeCardsCommand> validator, IArchetypeCardsRepository archetypeCardsRepository)
+        public UpdateArchetypeCardsCommandHandler(IValidator<UpdateArchetypeCardsCommand> validator, IArchetypeCardsService archetypeCardsService)
         {
             _validator = validator;
-            _archetypeCardsRepository = archetypeCardsRepository;
+            _archetypeCardsService = archetypeCardsService;
         }
         public async Task<CommandResult> Handle(UpdateArchetypeCardsCommand request, CancellationToken cancellationToken)
         {
@@ -27,12 +27,16 @@ namespace ygo.application.Commands.UpdateArchetypeCards
             {
                 if (request.Cards.Any())
                 {
-                    var result = await _archetypeCardsRepository.Update(request.ArchetypeId, request.Cards.Distinct());
+                    var result = await _archetypeCardsService.Update(request.ArchetypeId, request.Cards.Distinct());
 
                     commandResult.Data = result;
                 }
 
                 commandResult.IsSuccessful = true;
+            }
+            else
+            {
+                commandResult.Errors = validationResult.Errors.Select(err => err.ErrorMessage).ToList();
             }
 
             return commandResult;
---
+++ tests/unit/ygo.application.unit.tests/Commands/UpdateArchetypeCardsCommandHandlerTests.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System.Threading;
+using System.Threading.Tasks;
+using ygo.application.Commands.UpdateArchetypeCards;
+using ygo.core.Services;
+
+namespace ygo.application.unit.tests.Commands
+{
+    public class UpdateArchetypeCardsCommandHandlerTests
+    {
+        private IArchetypeCardsService _archetypeCardsService;
+        private UpdateArchetypeCardsCommandHandler _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _archetypeCardsService = Substitute.For<IArchetypeCardsService>();
+
+            _sut = new UpdateArchetypeCardsCommandHandler(new UpdateArchetypeCardsCommandValidator(), _archetypeCardsService);
+        }
+
+        [Test]
+        public async Task Given_An_Invalid_UpdateArchetypeCardsCommand_Validation_Should_Fail()
+        {
+            // Arrange
+            var command = new UpdateArchetypeCardsCommand();
+
+            // Act
+            var result = await _sut.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccessful.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task Given_An_Invalid_UpdateArchetypeCardsCommand_Validation_Should_Return_Error_List()
+        {
+            // Arrange
+            var command = new UpdateArchetypeCardsCommand();
+
+            // Act
+            var result = await _sut.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.Errors.Should().NotBeNullOrEmpty();
+        }
+
+
+        [Test]
+        public async Task Given_An_Valid_Archetype_With_No_Cards_Should_Not_Invoke_Update_Method()
+        {
+            // Arrange
+            var command = new UpdateArchetypeCardsCommand
+            {
+                Cards = new List<string>()
+            };
+
+            // Act
+            await _sut.Handle(command, CancellationToken.None);
+
+            // Assert
+            await _archetypeCardsService.DidNotReceive().Update(Arg.Any<long>(), Arg.Any<IEnumerable<string>>());
+        }
+
+        [Test]
+        public async Task Given_An_Valid_Archetype_With_Cards_Should_Invoke_Update_Method_Once()
+        {
+            // Arrange
+            var command = new UpdateArchetypeCardsCommand
+            {
+                ArchetypeId = 23423,
+                Cards = new List<string>
+                {
+                    "Monster Reborn",
+                    "Call Of The Haunted"
+                }
+            };
+
+            // Act
+            await _sut.Handle(command, CancellationToken.None);
+
+            // Assert
+            await _archetypeCardsService.Received(1).Update(Arg.Any<long>(), Arg.Any<IEnumerable<string>>());
+        }
+
+        [Test]
+        public async Task Given_An_Valid_Archetype_With_Cards_Command_Should_Execute_Successfully()
+        {
+            // Arrange
+            var command = new UpdateArchetypeCardsCommand
+            {
+                ArchetypeId = 23423,
+                Cards = new List<string>
+                {
+                    "Monster Reborn",
+                    "Call Of The Haunted"
+                }
+            };
+
+            // Act
+            var result = await _sut.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Given_An_Valid_Archetype_With_No_Cards_Command_Should_Execute_Successfully()
+        {
+            // Arrange
+            var command = new UpdateArchetypeCardsCommand
+            {
+                ArchetypeId = 2342,
+                Cards = new List<string>()
+            };
+
+            // Act
+            var result = await _sut.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.IsSuccessful.Should().BeTrue();
+        }
+    }
+}
