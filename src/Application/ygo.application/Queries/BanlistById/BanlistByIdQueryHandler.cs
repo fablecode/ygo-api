4e05e6530192008b46e38ed6d3eab7da2c9283e0
fablecode <fablecode@users.noreply.github.com>
2019-03-16 10:30:28 +0000

    BanlistByIdQueryHandler code coverage added.

--- src/Application/ygo.application/Queries/BanlistById/BanlistByIdQueryHandler.cs
+++ src/Application/ygo.application/Queries/BanlistById/BanlistByIdQueryHandler.cs
@@ -10,10 +10,12 @@ namespace ygo.application.Queries.BanlistById
     public class BanlistByIdQueryHandler : IRequestHandler<BanlistByIdQuery, BanlistDto>
     {
         private readonly IBanlistService _banlistService;
+        private readonly IMapper _mapper;
 
-        public BanlistByIdQueryHandler(IBanlistService banlistService)
+        public BanlistByIdQueryHandler(IBanlistService banlistService, IMapper mapper)
         {
             _banlistService = banlistService;
+            _mapper = mapper;
         }
 
         public async Task<BanlistDto> Handle(BanlistByIdQuery request, CancellationToken cancellationToken)
@@ -22,7 +24,7 @@ namespace ygo.application.Queries.BanlistById
 
             if (banlist != null)
             {
-                return Mapper.Map<BanlistDto>(banlist);
+                return _mapper.Map<BanlistDto>(banlist);
             }
 
             return null;
---
+++ tests/unit/ygo.application.unit.tests/QueriesTests/BanlistByIdQueryHandlerTests.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System.Threading;
+using System.Threading.Tasks;
+using ygo.application.Mappings.Profiles;
+using ygo.application.Queries.BanlistById;
+using ygo.core.Models.Db;
+using ygo.core.Services;
+using ygo.tests.core;
+
+namespace ygo.application.unit.tests.QueriesTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class BanlistByIdQueryHandlerTests
+    {
+        private BanlistByIdQueryHandler _sut;
+        private IBanlistService _banlistService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var config = new MapperConfiguration
+            (
+                cfg => { cfg.AddProfile(new BanlistProfile()); }
+            );
+
+            var mapper = config.CreateMapper();
+
+            _banlistService = Substitute.For<IBanlistService>();
+            _sut = new BanlistByIdQueryHandler(_banlistService, mapper);
+        }
+
+        [Test]
+        public async Task Given_An_BanlistId_If_Not_Found_Should_Return_Null()
+        {
+            // Arrange
+            _banlistService.GetBanlistById(Arg.Any<long>()).Returns(null as Banlist);
+
+            // Act
+            var result = await _sut.Handle(new BanlistByIdQuery(), CancellationToken.None);
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public async Task Given_An_BanlistId_If_Found_Should_Return_Banlist()
+        {
+            // Arrange
+            _banlistService.GetBanlistById(Arg.Any<long>()).Returns(new Banlist());
+
+            // Act
+            var result = await _sut.Handle(new BanlistByIdQuery(), CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        [Test]
+        public async Task Given_An_BanlistId_If_Found_Should_Execute_GetBanlistById_Method_Once()
+        {
+            // Arrange
+            _banlistService.GetBanlistById(Arg.Any<long>()).Returns(new Banlist());
+
+            // Act
+            await _sut.Handle(new BanlistByIdQuery(), CancellationToken.None);
+
+            // Assert
+            await _banlistService.Received(1).GetBanlistById(Arg.Any<long>());
+        }
+    }
+}
