d40ca351ee8c8ad8183fb9364ab6ef4fa9988af6
fablecode <fablecode@users.noreply.github.com>
2019-03-23 16:17:35 +0000

    AttributeService code coverage.

---
+++ tests/unit/ygo.domain.unit.tests/ServiceTests/AttributeServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using ygo.domain.Repository;
+using ygo.domain.Services;
+using ygo.tests.core;
+
+namespace ygo.domain.unit.tests.ServiceTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class AttributeServiceTests
+    {
+        private IAttributeRepository _attributeRepository;
+        private AttributeService _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _attributeRepository = Substitute.For<IAttributeRepository>();
+
+            _sut = new AttributeService(_attributeRepository);
+        }
+
+        [Test]
+        public async Task Should_Invoke_AllAttributes_Method_Once()
+        {
+            // Arrange
+            // Act
+            await _sut.AllAttributes();
+
+            // Assert
+            await _attributeRepository.Received(1).AllAttributes();
+        }
+    }
+}
