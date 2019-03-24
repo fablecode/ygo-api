57b38cf2bad4eede98bae4d429feec46958d4dcb
fablecode <fablecode@users.noreply.github.com>
2019-03-24 11:08:18 +0000

    FormatService code coverage.

---
+++ tests/unit/ygo.domain.unit.tests/ServiceTests/FormatServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using ygo.domain.Repository;
+using ygo.domain.Services;
+using ygo.tests.core;
+
+namespace ygo.domain.unit.tests.ServiceTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class FormatServiceTests
+    {
+        private IFormatRepository _formatRepository;
+        private FormatService _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _formatRepository = Substitute.For<IFormatRepository>();
+
+            _sut = new FormatService(_formatRepository);
+        }
+
+        [Test]
+        public void Given_An_Acronym_Should_Invoke_FormatByAcronym_Method_Once()
+        {
+            // Arrange
+            const string acronym = "TCG";
+
+            // Act
+            _sut.FormatByAcronym(acronym);
+
+            // Assert
+            _formatRepository.Received(1).FormatByAcronym(Arg.Is(acronym));
+        }
+    }
+}
