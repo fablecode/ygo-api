dee1a5391d733cd2e7e1a2ac332843a43fa83c0b
fablecode <fablecode@users.noreply.github.com>
2019-03-24 08:05:59 +0000

    CardTipService code coverage.

---
+++ tests/unit/ygo.domain.unit.tests/ServiceTests/CardTipServiceTests/TipSectionsByCardIdTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using ygo.domain.Repository;
+using ygo.domain.Services;
+using ygo.tests.core;
+
+namespace ygo.domain.unit.tests.ServiceTests.CardTipServiceTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class TipSectionsByCardIdTests
+    {
+        private ICardTipRepository _cardTipRepository;
+        private CardTipService _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cardTipRepository = Substitute.For<ICardTipRepository>();
+            _sut = new CardTipService(_cardTipRepository);
+        }
+
+        [Test]
+        public async Task Given_A_CardId_Should_Invoke_TipSectionsByCardId_Once()
+        {
+            // Arrange
+            const long cardId = 42342;
+
+            // Act
+            await _sut.TipSectionsByCardId(cardId);
+
+            // Assert
+            await _cardTipRepository.TipSectionsByCardId(Arg.Is(cardId));
+        }
+    }
+}
---
+++ tests/unit/ygo.domain.unit.tests/ServiceTests/CardTipServiceTests/UpdateTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using ygo.core.Models.Db;
+using ygo.domain.Repository;
+using ygo.domain.Services;
+using ygo.tests.core;
+
+namespace ygo.domain.unit.tests.ServiceTests.CardTipServiceTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class UpdateTests
+    {
+        private ICardTipRepository _cardTipRepository;
+        private CardTipService _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cardTipRepository = Substitute.For<ICardTipRepository>();
+            _sut = new CardTipService(_cardTipRepository);
+        }
+
+        [Test]
+        public async Task Given_TipSections_Should_Invoke_Update_Once()
+        {
+            // Arrange
+            var tipSections = new List<TipSection>();
+
+            // Act
+            await _sut.Update(tipSections);
+
+            // Assert
+            await _cardTipRepository.Update(Arg.Is(tipSections));
+        }
+    }
+}
