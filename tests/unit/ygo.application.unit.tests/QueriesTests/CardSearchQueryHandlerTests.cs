92473fc8373d34403d409ff190d511c2d6b658c2
fablecode <fablecode@users.noreply.github.com>
2019-09-14 20:44:14 +0100

    Code Coverage added.

---
+++ tests/unit/ygo.application.unit.tests/QueriesTests/CardSearchQueryHandlerTests.cs
@@ -0,0 +1,151 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using ygo.application.Configuration;
+using ygo.application.Dto;
+using ygo.application.Mappings.Resolvers;
+using ygo.application.Paging;
+using ygo.application.Queries.CardSearch;
+using ygo.core.Models;
+using ygo.core.Models.Db;
+using ygo.core.Services;
+using ygo.tests.core;
+
+namespace ygo.application.unit.tests.QueriesTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class CardSearchQueryHandlerTests
+    {
+        private ICardService _cardService;
+        private CardSearchQueryHandler _sut;
+        private IMapper _mapper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mapper = Substitute.For<IMapper>();
+
+            _cardService = Substitute.For<ICardService>();
+
+            _sut = new CardSearchQueryHandler(_cardService, new CardSearchQueryValidator(), _mapper);
+        }
+
+        [Test]
+        public async Task Given_An_Invalid_CardSearchQuery_Should_Not_Execute_Successfully()
+        {
+            // Arrange
+            var query = new CardSearchQuery();
+
+            // Act
+            var result = await _sut.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.IsSuccessful.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task Given_An_Invalid_CardSearchQuery_Should_Return_Error_List()
+        {
+            // Arrange
+            var query = new CardSearchQuery();
+
+            // Act
+            var result = await _sut.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.Errors.Should().NotBeNullOrEmpty();
+        }
+
+        [Test]
+        public async Task Given_A_Valid_CardSearchQuery_Should_Execute_Successfully()
+        {
+            // Arrange
+            var query = new CardSearchQuery
+            {
+                PageIndex = 1,
+                PageSize = 10
+            };
+
+            _cardService.Search(Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>()).Returns(new SearchResult<Card> { Items = new List<Card>()});
+
+            // Act
+            var result = await _sut.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Given_A_Valid_CardSearchQuery_Data_Should_Be_Of_Type_PagedList()
+        {
+            // Arrange
+            var query = new CardSearchQuery
+            {
+                PageIndex = 1,
+                PageSize = 10
+            };
+
+            _mapper.Map<CardImageEndpointResolver>(Arg.Any<Card>()).Returns(new CardImageEndpointResolver(Substitute.For<IOptions<ApplicationSettings>>()));
+
+            _cardService.Search(Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>()).Returns(new SearchResult<Card>{ Items = new List<Card>{new Card()}});
+
+            // Act
+            var result = await _sut.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.Data.Should().BeOfType<PagedList<CardDto>>();
+        }
+
+
+        [Test]
+        public async Task Given_A_Valid_CardSearchQuery_Data_Should_Archetype_List()
+        {
+            // Arrange
+            const int expected = 2;
+
+            var query = new CardSearchQuery
+            {
+                PageIndex = 1,
+                PageSize = 10
+            };
+
+            _cardService.Search(Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>()).Returns(new SearchResult<Card>{ Items = new List<Card>
+            {
+                new Card(),
+                new Card()
+            }});
+
+            // Act
+            var result = await _sut.Handle(query, CancellationToken.None);
+
+            // Assert
+            var pagedList =  (PagedList<CardDto>)result.Data;
+            pagedList.List.Should().HaveCount(expected);
+        }
+
+        [Test]
+        public async Task Given_A_Valid_CardSearchQuery_Should_Execute_Search_Method_Once()
+        {
+            // Arrange
+            var query = new CardSearchQuery
+            {
+                PageIndex = 1,
+                PageSize = 10
+            };
+
+            _cardService.Search(Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>()).Returns(new SearchResult<Card> { Items = new List<Card>()});
+
+            // Act
+            await _sut.Handle(query, CancellationToken.None);
+
+            // Assert
+            await _cardService.Received(1).Search(Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>());
+        }
+    }
+}
---
+++ tests/unit/ygo.domain.unit.tests/ServiceTests/CardServiceTests/SearchTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using ygo.core.Models;
+using ygo.core.Models.Db;
+using ygo.core.Strategies;
+using ygo.domain.Repository;
+using ygo.domain.Services;
+using ygo.tests.core;
+
+namespace ygo.domain.unit.tests.ServiceTests.CardServiceTests
+{
+    [TestFixture]
+    [Category(TestType.Unit)]
+    public class SearchTests
+    {
+        private IEnumerable<ICardTypeStrategy> _cardTypeStrategies;
+        private ICardRepository _cardRepository;
+        private CardService _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cardTypeStrategies = Substitute.For<IEnumerable<ICardTypeStrategy>>();
+            _cardRepository = Substitute.For<ICardRepository>();
+            _sut = new CardService(_cardTypeStrategies, _cardRepository);
+        }
+
+        [Test]
+        public async Task Given_A_Card_Id_Should_Invoke_Search_Method_Once()
+        {
+            // Arrange
+            const string searchTerm = "query";
+            const int pageIndex = 1;
+            const int pageSize = 10;
+
+            _cardRepository.Search(Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>()).Returns(new SearchResult<Card>());
+
+            // Act
+            await _sut.Search(searchTerm, pageIndex, pageSize);
+
+            // Assert
+            await _cardRepository.Received(1).Search(Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>());
+        }
+    }
+}
